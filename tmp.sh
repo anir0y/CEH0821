e58023a4845b27431d57f71a0ce6f0ae505fc28a
VM-CEH0821 <root@anir0y.com>
2021-08-26 22:42:31 +0530

    auto push at Aug 26 2021 by CEH-VM0821 for reason:

--- tmp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-echo "What's new?: "
-read reason
-echo "auto push at $(date)" by $(whoami) for reason: $reason |tee pubmsg
-git add --all
-git commit -F pubmsg
-git push -u origin main
-
