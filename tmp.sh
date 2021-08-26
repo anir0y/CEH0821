3531c4c19b65e6b3715346c3b13c77ed7c9a6bce
VM-CEH0821 <root@anir0y.com>
2021-08-26 22:31:29 +0530

    auto push at Thursday 26 August 2021 10:31:29 PM IST by kali for reason: testing

--- tmp.sh
+++ tmp.sh
@@ -1,7 +1,9 @@
 #!/bin/zsh
 
-commit='auto push at $(date) by $(whoami)'
+echo "What's new?: "
+read reason
+echo "auto push at $(date)" by $(whoami) for reason: $reason |tee pubmsg
 git add --all
-git commit -m '$commit'
+git commit -F pubmsg
 git push -u origin main
 
